8f0c14016ad4f6e4a642cff768a03de95ccc3381
JAGHUARD <dammy@jaghuard.dev>
2024-03-26 12:31:29 +0100

    Create automated tests for addToCart

--- Tests/moneyTest.js
+++ Tests/moneyTest.js
@@ -5,7 +5,7 @@ console.log('test suite: formatCurrency')
 console.log('converts cents into dollars')
 
 if (formatCurrency(2095) === '20.95') {
-    console.log('passed');
+    console.log('passed ');
 } else{
     console.log('failed');
 }
